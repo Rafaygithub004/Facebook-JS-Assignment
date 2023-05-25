6ffd42a84134edb1bcf8fd886b2606cc8bcdd517
Rafaygithub004 <rafayabdur004@gmail.com>
2023-05-25 10:28:39 -0700

    your commit message

---
+++ app.js
@@ -0,0 +1,24 @@
+var in1 = document.getElementById("firstname");
+var in2 = document.getElementById("surname");
+var in3 = document.getElementById("email");
+var in4 = document.getElementById("password");
+var in5 = document.getElementById("day");
+var in6 = document.getElementById("month");
+var in7 = document.getElementById("year");
+var in8 = document.getElementById("gender");
+
+
+
+
+
+function signup(){
+    console.log("firstname :"+ in1.value);
+    console.log("surname :"+in2.value);
+    console.log("email :"+in3.value);
+    console.log("password :"+in4.value);
+    console.log("day of birth :"+in5.value);
+    console.log( "month of birth :"+in6.value);
+    console.log( "year of birth :"+in7.value);
+    console.log("gender :"+in8.value);
+
+}
